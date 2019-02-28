bba03b52137e0b88a17ea60838159f26400b2205
Mschweda <tahoe27@gmail.com>
2019-02-27 23:16:19 -0600

    js

---
+++ myPortfolio/js/ticTacToe.js
@@ -0,0 +1,12 @@
+var gameMarker = "x"
+function changeMarkerToX(x){
+    console.log("The x button was clicked!")
+}
+
+var gameMarker = "o"
+function changeMarkerToO(o){
+    console.log("The x button was clicked!") 
+}
+
+function placeMark(){}
+
