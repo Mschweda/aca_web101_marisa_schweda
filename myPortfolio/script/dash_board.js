22c634a2c1cf6952da71ea0344f019a925bfaf7c
Mschweda <tahoe27@gmail.com>
2019-03-06 02:50:46 -0600

    DB

---
+++ myPortfolio/script/dash_board.js
@@ -0,0 +1,72 @@
+
+
+$(function() {
+    let $list = $('ul');
+    let $newItemForm = $('#newItemForm');
+
+    $newItemForm.on('submit', function(e) {
+      e.preventDefault();
+      let text = $('input[type="text"]').val();
+      $list.append(`<li>${text}</li>`);
+      $('input[type="text"]').val('');
+    });
+  
+    $list.on('click', 'li', function() {
+      let $this = $(this);
+      $this.remove();
+    });
+});
+
+var myLocation = document.getElementById("my-location")
+
+function getLocation() {
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(displayPosition)
+  } else {
+   myLocation.innerHTML = "Not available"
+  }
+}
+
+function displayPosition(position) {
+  myLocation.innerHTML = "Longitude: " + position.coords.longitude + " Latitude: " + position.coords.latitude
+}
+function renderTime(){
+  var mydate = new Date();
+  var year = mydate.getYear();
+    if(year < 1000){
+        year +=1900;
+     } 
+  var day = mydate.getDay();
+  var month = mydate.getMonth();
+  var daym = mydate.getDate ();
+  var dayarray = new Array ("Sunday",   "Monday", "Tuesday", "Wesnesday",     "Thursday", "Friday", "Saturday");
+  var montharray= new Array("January",    "February", "March", "April",         "May", "June", "July", "August", "September", "October", "November",       "December");
+  
+  // date end
+  //Time
+  var currentTime = new Date();
+  var h = currentTime.getHours();
+  var m = currentTime.getMinutes();
+  var s = currentTime.getSeconds();
+  if(h == 24){
+    h = 0;
+} else if(h > 12){
+    h = h - 0;
+}
+
+if(m < 10){
+  m = "0" + m;
+}
+
+if(s < 10){
+  s = "0" + s;
+}
+    var myClock = document.getElementById("clockDisplay");
+    myClock.textContext = "" + dayarray[day]+ "  " +montharray[month]+ " "  +daym+ " "+year+ " | " +h+ ":" +m+ ":" +s;
+    myClock.innerText =  "" + dayarray[day]+ "  " +montharray[month]+ " "  +daym+ ", " +year+ "  " +h+ ":" +m+ ":" +s;
+  
+      setTimeout('renderTime()', 1000)
+}
+
+                        
+  
